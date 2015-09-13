8de8d6a4d1072391d5b3dddf1345366205362fd6
Luke Phillips <luke@femurdesign.com>
2015-09-13 11:23:04 +0100

    echo favicon directory

--- wp-content/themes/KollektivGallery/header.php
+++ wp-content/themes/KollektivGallery/header.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title><? wp_title( '|', true, 'right' ); ?></title>
-	    <link rel="shortcut icon" href="<? get_template_directory_uri(); ?>/favicon.png">
+	    <link rel="shortcut icon" href="<?= get_template_directory_uri(); ?>/favicon.png">
         <? wp_head(); ?>
     </head>
 
